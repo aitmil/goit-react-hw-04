70ddc2fa7896ec1a4a2c10136c7ef266f967ea66
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-19 20:21:34 +0300

    update search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,30 @@
+import { Formik, Form, Field } from "formik";
+import toast, { Toaster } from "react-hot-toast";
 import css from "./SearchBar.module.css";
 
-export default function SearchBar() {
-  return <></>;
+export default function SearchBar({ onSubmit }) {
+  const notify = () => toast.error("Please fill in the field!");
+  const handleSubmit = (values, actions) => {
+    values.search === "" ? notify() : onSubmit(values.search);
+    actions.resetForm();
+  };
+  return (
+    <header className="css.header">
+      <Formik initialValues={{ search: "" }} onSubmit={handleSubmit}>
+        <Form className={css.form}>
+          <Field
+            className={css.input}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="search"
+          />
+          <button type="submit" className={css.btn}>
+            Search
+          </button>
+        </Form>
+      </Formik>
+      <Toaster />
+    </header>
+  );
 }
