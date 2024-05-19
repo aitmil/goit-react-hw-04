174677e7eb8f098251181d1d80b9c87038625829
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-19 20:22:42 +0300

    add error message

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 import css from "./ErrorMessage.module.css";
 
 export default function ErrorMessage() {
-  return <></>;
+  return <p className={css.text}>Oops! There was an error! Try again!</p>;
 }
