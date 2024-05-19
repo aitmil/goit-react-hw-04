af6b8424c127ae1e1c10f67a7edcc1e7f8efde8a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-19 20:21:52 +0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,19 @@
+import { ThreeDots } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
-export default function Loader() {
-  return <></>;
+export default function Loader({ isLoading }) {
+  return (
+    <>
+      <ThreeDots
+        visible={isLoading}
+        height="80"
+        width="80"
+        color="#4fa94d"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </>
+  );
 }
