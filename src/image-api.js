ae747f38f6bd90b509fe4a22f2144a7ac2897f7a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-19 20:21:14 +0300

    add api request

---
+++ src/image-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const myApiKey = "j_uEFQ_tCj9u552Vl8G3rL2Knowhn856-NdsV7ba6Ww";
+
+axios.defaults.headers.common["Authorization"] = `Client-ID ${myApiKey}`;
+axios.defaults.baseURL = "https://api.unsplash.com/";
+
+export const getImages = async (query, currentPage) => {
+  const response = await axios.get("/search/photos", {
+    params: {
+      query: query,
+      page: currentPage,
+      orientation: "landscape",
+    },
+  });
+  return response.data.results;
+};
